4835615486e75ccf0e67ac85c688ae651d6acffe
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-02-16 10:37:10 +0430

    setup the project with react , vite , tailwind Css

---
+++ Fontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-gray-200">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis porro
+      distinctio tenetur ipsam ea, expedita obcaecati. Id minus alias autem
+      error. Praesentium placeat ducimus sequi sit soluta officia quaerat quod.
+    </div>
+  );
+};
+
+export default App;
